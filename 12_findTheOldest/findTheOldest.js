92a7706594d996cbf7f8b5f63a153b4854728d3f
getDub <143089345+getDub@users.noreply.github.com>
2023-11-28 09:08:34 +1100

    12 find the Oldest. Another interesting way to approach reduce

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
 
+return arr.reduce((oldest, currentPerson) => {
+const oldestAge = age(oldest.yearOfBirth, oldest.yearOfDeath);
+const currentAge = age(currentPerson.yearOfBirth, currentPerson.yearOfDeath )
+return oldestAge < currentAge ? currentPerson : oldest;
+});
 };
+
+const age = function (born, died){
+
+    if(!died){
+       died = new Date();
+       return died.getFullYear() - born;
+    }
+    
+    return died - born
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
