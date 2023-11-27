3288d54701f1ef0acf3fe7717deede917a5ac0e5
getDub <143089345+getDub@users.noreply.github.com>
2023-11-27 17:04:59 +1100

    10 Fibonacci completed. Iterates over n-1 and n-2  to arrive at Fn. Control is the index parameter. Index starts at 2 becasue n-1 and n-2 have been declared like the Fib equation

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(indx) {
+    if( indx < 0 ) return "nope";
+    if( indx === 0) return 0;
+
+    let firstPrevNo = 1, secondPrevNo = 0, currentNo;
+    // i starts at 2 because firstPrevNo and secondPrevNo's have been declared
+    for( let i = 2; i <= indx; i++){
+        currentNo = firstPrevNo + secondPrevNo;
+        console.log(`current = ${currentNo}`)
+        secondPrevNo = firstPrevNo;
+        firstPrevNo = currentNo
+    }
+    return firstPrevNo;
 
 };
 
