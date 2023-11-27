e2d9d76556d761597e4b390ad62373c80cce6fc8
getDub <143089345+getDub@users.noreply.github.com>
2023-11-27 12:08:50 +1100

    09 with intro to RegEx

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -4,6 +4,12 @@ const palindromes = function (str) {
     return xStr.split("").reverse().join("") === xStr;
 };
 
+// intro to regex - everything that is not a-z or 0-9 replace with nothing.
+const palindromesREGEX = function (str) {
+    
+    let processedString = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+    return processedString.split("").reverse().join("") === processedString
+};
 
 // Do not edit below this line
 module.exports = palindromes;
